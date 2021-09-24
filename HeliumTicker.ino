6fe327259236a75e2f062bca4b841af719fadb18
nwpeckham88 <74386527+nwpeckham88@users.noreply.github.com>
2021-09-23 21:47:21 -0500

    Added OTA code

--- HeliumTicker.ino
+++ HeliumTicker.ino
@@ -19,6 +19,9 @@
 #define PSTR // Make Arduino Due happy
 #endif
 
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
 #define PIN 5
 
 #define WIFI_SSID "CenturyLink5739"
@@ -109,21 +112,57 @@ void setup() {
   server.begin();
   Serial.println("HTTP server started.");
 
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname("heliumticker");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+
   Serial.println("ready!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   unsigned long now = millis();
-
-  server.handleClient();
-
-  matrix.fillScreen(0);
-  matrix.setCursor(0, 0);
-  matrix.print(F("Howdy"));
-  matrix.show();
-  delay(100);
-
   if (now - last_wifi_check_time > WIFI_TIMEOUT) {
     Serial.print("Checking WiFi... ");
     if (WiFi.status() != WL_CONNECTED) {
@@ -134,6 +173,16 @@ void loop() {
     }
     last_wifi_check_time = now;
   }
+  ArduinoOTA.handle();
+  // put your main code here, to run repeatedly:
+
+  server.handleClient();
+
+  matrix.fillScreen(0);
+  matrix.setCursor(0, 0);
+  matrix.print(F("Howdy"));
+  matrix.show();
+  delay(100);
 }
 
 /*
