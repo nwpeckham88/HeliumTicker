3642236464b9859cb5dd6778b095c879c1733da6
nwpeckham88 <74386527+nwpeckham88@users.noreply.github.com>
2021-10-26 05:13:47 -0500

    Moved sensitive info to sensitive.h

--- HeliumTicker.ino
+++ HeliumTicker.ino
@@ -31,8 +31,7 @@ HTTPClient http;
 
 #define PIN D5
 
-#define WIFI_SSID "CenturyLink5739"
-#define WIFI_PASSWORD "jjugy4z5jdnw76"
+#include "sensitive.h"
 
 #define DEFAULT_COLOR 0xAA59AA
 #define DEFAULT_BRIGHTNESS 128
@@ -82,9 +81,6 @@ HTTPClient http;
 
 extern const char index_html[];
 extern const char main_js[];
-
-const String HOTSPOT_ADDRESS = "112bdGoWiDD9FTfTMHt2xgbAY2mn6dEULjGxwpZfJfhQRS9TYRGx";
-const String ACCOUNT_ADDRESS = "14so6GT2PBAkttTL1askWJ2NfdYQycj9JhLTmVoPDSj7nRvTyAD";
 
 // QUICKFIX...See https://github.com/esp8266/Arduino/issues/263
 #define min(a,b) ((a)<(b)?(a):(b))
